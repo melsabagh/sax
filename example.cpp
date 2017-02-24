9918dcfa00a5f5a65fe145dbd453c844260e659f
melsabagh <melsabagh@users.noreply.github.com>
2017-02-23 20:21:06 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,30 +1,23 @@
 #include <vector>
+#include <string>
 #include <iostream>
 #include "saxquantizer.hpp"
 
-int main(int argc, char **argv) {
+using namespace std;
 
+int main(int argc, char **argv) {
   SaxQuantizer::Sax sax(6, 1, 5);
   vector<double> input = { 1, 1, 1, 1, 2, 2, 10, 10, 100 };
   vector<int> output;
   sax.quantize(input, &output, false); // true for reduction
 
-  std::string delim = "";
-  std::cout << "input: ";
-  for (auto x : input) {
-    std::cout << delim << x;
-    delim = ", ";
-  }
-  std::cout << std::endl;
+  string delim = "";
+  cout << "input:";
+  for (const auto & x : input) cout << " " << x;
 
-  delim = "";
-  std::cout << "output: ";
-  for (auto x : output) {
-    std::cout << delim << x;
-    delim = ", ";
-  }
-  std::cout << std::endl;
+  cout << endl;
 
+  cout << "output:";
+  for (const auto & x : output) cout << " " << x;
+  cout << endl;
 }
-
-
